2c1c4baa2fcd996709cb38f6d14780de784d3052
Azhar uddin <azharuddin@192.168.2.101>
2022-08-26 14:44:11 +0530

    removed sqlite (nouse) commented auth provided for now which make page refreshing infinite long

--- vmasfrontend/src/index.tsx
+++ vmasfrontend/src/index.tsx
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <AuthProvider>
+    {/* <AuthProvider> */}
       <App />
-    </AuthProvider>
+    {/* </AuthProvider>/ */}
   </React.StrictMode>
 );
 
